bee658650c927f2b5950769b53c506536b98e38c
rzeszut <rzeszutekm@gmail.com>
2014-02-01 20:01:48 +0100

    Minor vim config improvements; added ycm_extra_conf

--- scripts/install/install_vim.sh
+++ scripts/install/install_vim.sh
@@ -3,23 +3,24 @@
 CFGROOT=$(pwd)
 
 echo "Installing vim ..."
-ln -fs ${CFGROOT}/vim/vimrc ${HOME}/.vimrc
+ln -fs $CFGROOT/vim/vimrc $HOME/.vimrc
 
 echo "Installing Vundle ..."
-mkdir -p ${HOME}/.vim/bundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+mkdir -p $HOME/.vim/bundle
+git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 
 echo "Downloading plugins ..."
 vim +BundleClean! +BundleInstall! +qall
 
 echo "Compiling YCM ..."
-cd ${HOME}/.vim/bundle/YouCompleteMe
+cd $HOME/.vim/bundle/YouCompleteMe
 ./install.sh --clang-completer
+ln -fs $CFGROOT/vim/ycm_extra_conf.py $HOME/.ycm_extra_conf.py
 
 if [[ ! -z `command -v npm` ]]; then
     echo "Installing tern ..."
-    cd ${HOME}/.vim/bundle/tern_for_vim
+    cd $HOME/.vim/bundle/tern_for_vim
     npm install
 fi
 
-cd ${CFGROOT}
+cd $CFGROOT
