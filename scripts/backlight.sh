8b1a713aa2617b3a50b8a48523957f65dc9d0fb3
rzeszut <rzeszutekm@gmail.com>
2015-08-01 18:28:14 +0200

    Improved backlight handling in i3

---
+++ scripts/backlight.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+HOSTNAME=$(hostname)
+if [[ $HOSTNAME == *netbook* ]]; then
+    # Intel graphics card on netbook can only change its brightness by ~15%
+    BACKLIGHT_STEP_UP=15
+    BACKLIGHT_STEP_DOWN=10
+else
+    BACKLIGHT_STEP_UP=3
+    BACKLIGHT_STEP_DOWN=3
+fi
+
+case $1 in
+    up)
+        xbacklight + $BACKLIGHT_STEP_UP
+        ;;
+
+    down)
+        xbacklight - $BACKLIGHT_STEP_DOWN
+        ;;
+
+    *)
+        echo "Usage: $(basename $0) (up|down)"
+esac
+
